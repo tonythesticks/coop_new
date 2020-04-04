fd3b28fc6d9ca920c883aa693d8f5d4cd71a8b58
53645714n <schoenmaker@mailbox.org>
2020-04-04 12:35:48 +0200

    Fixed led.py

--- tests/led.py
+++ tests/led.py
@@ -1,13 +1,20 @@
 import RPi.GPIO as GPIO
 import time
+
+RED = 10
+GREEN = 12
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(12,GPIO.OUT)
-GPIO.setup(10,GPIO.OUT)
-print "Door open"
-GPIO.output(12,GPIO.HIGH)
-GPIO.output(10,GPIO.LOW)
+GPIO.setup(GREEN,GPIO.OUT)
+GPIO.setup(RED,GPIO.OUT)
+print "Green LED for 10 seconds"
+GPIO.output(GREEN,GPIO.HIGH)
+GPIO.output(RED,GPIO.LOW)
 time.sleep(10)
-print "Door closed"
-GPIO.output(12,GPIO.LOW)
-GPIO.output(10,GPIO.HIGH)
+print "Red LED for 10 seconds"
+GPIO.output(GREEN,GPIO.LOW)
+GPIO.output(RED,GPIO.HIGH)
+time.sleep(10)
+
+GPIO.cleanup()
