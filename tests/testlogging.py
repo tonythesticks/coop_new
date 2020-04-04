331a2763273201b6d08e85c32b4d6d310ba2476f
Daniël Schoenmaker <44052459+53645714n@users.noreply.github.com>
2020-04-04 20:49:25 +0200

    Added date and level to logging

--- tests/testlogging.py
+++ tests/testlogging.py
@@ -1,5 +1,5 @@
 import logging
 
-logging.basicConfig(filename='coop.log', level=logging.DEBUG)
+logging.basicConfig(filename='coop.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s')
 
 logging.info('Test logging')
